bc6b2b8348fb1e68f623c46138f1fde24d408dc9
Danny Andrews <lunde.andrews@gmail.com>
2024-04-11 16:02:35 -0400

    Various style fixes

---
+++ src/components/Subjects/Subjects.jsx
@@ -0,0 +1,35 @@
+import { h, Fragment } from "preact";
+import { memo } from "preact/compat";
+import { useRef, useEffect } from "preact/hooks";
+import hljs from "highlight.js/lib/core";
+
+const Subjects = memo(({ subjects }) => {
+  const rootRef = useRef(null);
+
+  useEffect(() => {
+    for (let element of rootRef.current.querySelectorAll("pre code")) {
+      hljs.highlightElement(element);
+    }
+    rootRef.current.show(subjects[0].name);
+  }, [subjects]);
+
+  return (
+    <sl-tab-group ref={rootRef}>
+      {subjects.map((subject) => (
+        <sl-tab key={subject.name} slot="nav" panel={subject.name} selected>
+          {subject.name}
+        </sl-tab>
+      ))}
+
+      {subjects.map((subject) => (
+        <sl-tab-panel key={subject.name} name={subject.name}>
+          <pre>
+            <code>{subject.toString()}</code>
+          </pre>
+        </sl-tab-panel>
+      ))}
+    </sl-tab-group>
+  );
+});
+
+export default Subjects;
