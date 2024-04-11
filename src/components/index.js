f415eacd375225dc5c634488bf1abc42687c0613
Danny Andrews <lunde.andrews@gmail.com>
2024-04-11 16:03:41 -0400

    Remove unused import

--- src/components/index.js
+++ src/components/index.js
@@ -9,4 +9,3 @@ import "@shoelace-style/shoelace/dist/components/tooltip/tooltip.js";
 import "@shoelace-style/shoelace/dist/components/tab-group/tab-group.js";
 import "@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js";
 import "@shoelace-style/shoelace/dist/components/tab/tab.js";
-import "@shoelace-style/shoelace/dist/components/details/details.js";
