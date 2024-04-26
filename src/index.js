55981b48b07e1ce6e0a0f25e147a5232203fdb57
Danny Andrews <lunde.andrews@gmail.com>
2024-04-26 10:48:59 -0400

    Fix label spacing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./components/index.js";
+import "./styles/app.css";
 import App from "./components/App/App.jsx";
 import { render, h } from "preact";
 import hljs from "highlight.js/lib/core";
