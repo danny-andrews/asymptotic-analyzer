1837ce05b0a46d8737b9baf84a9602631c277d72
Danny Andrews <lunde.andrews@gmail.com>
2024-04-30 16:59:08 -0400

    Format bytes for human readability
    Fixes #25.

--- src/shared/util.js
+++ src/shared/util.js
@@ -20,3 +20,21 @@ export const throttle = (cb, delay) => {
     }, delay);
   };
 };
+
+export const formatBytes = (bytes) => {
+  const bytePrefixes = ["", "kilo", "mega", "giga", "tera", "peta"];
+  const index = Math.max(
+    0,
+    Math.min(Math.floor(Math.log10(bytes) / 3), bytePrefixes.length - 1)
+  );
+  const unit = bytePrefixes[index] + "byte";
+  const valueToFormat = parseFloat(
+    (bytes / Math.pow(1000, index)).toPrecision(3)
+  );
+
+  return new Intl.NumberFormat("en", {
+    style: "unit",
+    unit,
+    unitDisplay: "short",
+  }).format(valueToFormat);
+};
