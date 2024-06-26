ea1eddb7e6870fc7256b3ce1dbe444162acc45f0
Danny Andrews <lunde.andrews@gmail.com>
2024-06-26 15:22:20 -0400

    Optimize for-loop range function

--- core/test/algorithms/range.ts
+++ core/test/algorithms/range.ts
@@ -13,7 +13,7 @@ export const from = (start: number, length: number, step = 1) =>
   Array.from({ length }, (_, i) => (i + start) * step);
 
 export const forLoop = (start: number, length: number, step = 1) => {
-  const result = [];
+  const result = Array(length);
 
   for (let i = 0; i < length; i++) {
     result.push((i + start) * step);
